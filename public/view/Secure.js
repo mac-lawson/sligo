642599fec4969649da0e38adeb770da81d557fb1
Mac L <macattackairman@gmail.com>
2022-11-07 21:59:35 -0500

    working

--- public/view/Secure.js
+++ public/view/Secure.js
@@ -3,7 +3,7 @@ const { doc } = require("prettier");
  * <h1>Secure the Room</h1>
  */
 function secureRoom() {
-    handleVideo(e, false);
+    // handleVideo(e, false);
     const Confidential = document.getElementById("Confidential");
     const Top_Secret = document.getElementById("Top Secret");
 
@@ -14,7 +14,7 @@ function secureRoom() {
  * <h1>Unsecure the Room</h1>
  */
 function unencryptRoom() {
-    handleVideo(e, true);
+    // handleVideo(e, true);
     const Confidential = document.getElementById("Confidential");
     const Top_Secret = document.getElementById("Top Secret");
 
