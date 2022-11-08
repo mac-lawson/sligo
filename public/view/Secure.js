12ea7d50ffccd31f58b81732f93f3ac0cc12c5a7
Mac L <macattackairman@gmail.com>
2022-11-07 22:14:14 -0500

    w

--- public/view/Secure.js
+++ public/view/Secure.js
@@ -1,22 +1,17 @@
-const { doc } = require("prettier");
-/**
- * <h1>Secure the Room</h1>
- */
+
 function secureRoom() {
     // handleVideo(e, false);
-    const Confidential = document.getElementById("Confidential");
-    const Top_Secret = document.getElementById("Top Secret");
+    const Confidential = document.getElementById('Confidential');
+    const Top_Secret = document.getElementById('Top Secret');
 
     Confidential.textContent == "";
     Top_Secret.textContent == "Top Secret";
 }
-/**
- * <h1>Unsecure the Room</h1>
- */
+
 function unencryptRoom() {
     // handleVideo(e, true);
-    const Confidential = document.getElementById("Confidential");
-    const Top_Secret = document.getElementById("Top Secret");
+    const Confidential = document.getElementById('Confidential');
+    const Top_Secret = document.getElementById('Top Secret');
 
     Confidential.textContent == "Confidential";
     Top_Secret.textContent == "";
