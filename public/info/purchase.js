0161fa73207e465adddabc7eb980083cd4053500
Mac L <macattackairman@gmail.com>
2022-11-09 20:58:20 -0500

    public site

--- public/info/purchase.js
+++ public/info/purchase.js
@@ -1,6 +1,14 @@
 function free() {
     window.open("https://github.com/mac-lawson/sligo");
 }
-function free() {
-    window.open("https://github.com/mac-lawson/sligo");
+function pro() {
+    window.open("https://unlock.sh/download/sligo-professional#");
+}
+function contact() {
+    window.open("mailto:mlawson07@protonmail.com?subject=LICENSE%3A%20Enterprise%20Sligo%20Edition&body=--%20START%20MESSAGE%20--");
+}
+function deploy(){
+    window.open('./deploy.html',
+    '_blank',
+    'width=400,height=400 top=200,left=600');
 }
